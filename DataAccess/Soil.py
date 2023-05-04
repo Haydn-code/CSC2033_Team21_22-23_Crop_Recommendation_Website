fbc3389b6ea6ad8919c64b72f809445266abaf6b
Haydn Small <91317802+Haydn-code@users.noreply.github.com>
2023-05-04 22:35:16 +0100

    added some variables to coordsToPixels as this is better code practice and makes my code more readable for others, also started working on a way to get the soil profiles for each map code. Added an extra parameter to getSoilData that will allow for simple and more detailed access.

--- DataAccess/Soil.py
+++ DataAccess/Soil.py
@@ -1,31 +1,33 @@
 from osgeo import gdal
 import pandas as pd
 
-
 """Takes longitudinal and latitudinal coords and returns the location of the pixels they correspond to in the raster 
 file"""
-def coordsToPixels(long, lat):
-    assert -180 <= long <= 180 and -90 <= lat <= 90
-    lat_idx = int((lat - -90) / (90 - -90) * 16753)
-    long_idx = int((long - -180) / (180 - -180) * 43201)
+
+
+def coordsToPixels(long, lat, rows, cols):
+    long_max = 180
+    long_min = -180
+    lat_max = 90
+    lat_min = -90
+    assert long_min <= long <= long_max and lat_min <= lat <= lat_max
+    lat_idx = int((lat - lat_min) / (lat_max - lat_min) * rows)
+    long_idx = int((long - long_min) / (long_max - long_min) * cols)
     return long_idx, lat_idx
 
 
-print(coordsToPixels(0, -90))
-
-
 """incomplete currently just displays some generic data about raster files"""
-def getSoilData(long, lat):
-    assert -180 <= long <= 180 and -90 <= lat <= 90
+
+
+def getSoilData(long, lat, detailed):
     # finds coordinates from raster file
     raster = gdal.Open("WISE30sec/Interchangeable_format/wise_30sec_v1.tif")
-    rows = raster.RasterXSize
-    cols = raster.RasterYSize
+    rows = raster.RasterYSize
+    cols = raster.RasterXSize
     # prints dimensions of raster file
-    print(rows)
-    print(cols)
+    print(cols, rows)
     band = raster.GetRasterBand(1)
-    data = band.ReadAsArray(0, 0, rows, cols)
+    data = band.ReadAsArray(0, 0, cols, rows)
     # prints the values of pixels near the start and end of raster file
     print(data)
 
@@ -33,4 +35,19 @@ def getSoilData(long, lat):
     df = pd.read_csv('WISE30sec/Interchangeable_format/wise_30sec_v1.tsv', sep='\t')
     print(df)
 
-getSoilData(0,0)
+    # finds the map code associated with the pixel
+    x, y = coordsToPixels(long, lat, rows, cols)
+    print(data[y][x])
+    pixel = df.loc[df["pixel_vaue"] == data[y][x]]
+    print(pixel)
+    map_code = pixel.get("description")
+
+    # finds the profile ID of the corresponding map code
+    df1 = pd.read_csv('WISE30sec/Interchangeable_format/HW30s_MapUnit.txt', sep=',', dtype=str)
+    print(df1)
+    print(str(map_code)[7:17])
+    soil_record = df1.loc[df1["NEWSUID"] == str(map_code)[7:17]]
+    profile_id = soil_record.get()
+
+
+getSoilData(0, 0, 1)
