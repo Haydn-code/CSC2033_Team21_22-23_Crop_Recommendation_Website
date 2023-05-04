8ce08fffb667e345070c9e91927a038ce13b20da
Haydn Small <91317802+Haydn-code@users.noreply.github.com>
2023-05-04 15:30:40 +0100

    Created a method which transforms longitudinal and latitudinal coords into the location of their corresponding pixel in a raster file and started programming a method getSoilData which simply just displays some generic data about raster files at the moment.

--- DataAccess/Soil.py
+++ DataAccess/Soil.py
@@ -1,1 +1,35 @@
 from osgeo import gdal
+import pandas as pd
+
+
+"""Takes longitudinal and latitudinal coords and returns the location of the pixels they correspond to in the raster 
+file"""
+def coordsToPixels(long, lat):
+    assert -180 <= long <= 180 and -90 <= lat <= 90
+    lat_idx = int((lat - -90) / (90 - -90) * 16753)
+    long_idx = int((long - -180) / (180 - -180) * 43201)
+    return long_idx, lat_idx
+
+
+print(coordsToPixels(0, -90))
+
+
+"""incomplete currently just displays some generic data about raster files"""
+def getSoilData(long, lat):
+    assert -180 <= long <= 180 and -90 <= lat <= 90
+    # finds coordinates from raster file
+    raster = gdal.Open("WISE30sec/Interchangeable_format/wise_30sec_v1.tif")
+    rows = raster.RasterXSize
+    cols = raster.RasterYSize
+    print(rows)
+    print(cols)
+    band = raster.GetRasterBand(1)
+    data = band.ReadAsArray(0, 0, rows, cols)
+    print(band)
+    print(data)
+
+    # opens a .tsv file that can be used to
+    df = pd.read_csv('WISE30sec/Interchangeable_format/wise_30sec_v1.tsv', sep='\t')
+    print(df)
+
+getSoilData(0,0)
