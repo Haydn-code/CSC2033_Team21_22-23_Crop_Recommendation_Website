2635adb7d85cdae45b1a8a00c07ee12aa71ef637
Haydn Small <91317802+Haydn-code@users.noreply.github.com>
2023-05-05 09:50:47 +0100

    updated some variable names so that the code is easier to read and close to being access the soil data. Decided on creating a readProfile function to reduce code duplication.

--- DataAccess/Soil.py
+++ DataAccess/Soil.py
@@ -32,22 +32,43 @@ def getSoilData(long, lat, detailed):
     print(data)
 
     # opens a .tsv file that can be used to gather some metadata about the raster file
-    df = pd.read_csv('WISE30sec/Interchangeable_format/wise_30sec_v1.tsv', sep='\t')
-    print(df)
+    pixels = pd.read_csv('WISE30sec/Interchangeable_format/wise_30sec_v1.tsv', sep='\t')
+    print(pixels)
 
     # finds the map code associated with the pixel
     x, y = coordsToPixels(long, lat, rows, cols)
     print(data[y][x])
-    pixel = df.loc[df["pixel_vaue"] == data[y][x]]
-    print(pixel)
-    map_code = pixel.get("description")
+    pixel_value = pixels.loc[pixels["pixel_vaue"] == data[y][x]]
+    print(pixel_value)
+    map_code = str(pixel_value.get("description"))[7:17]
 
     # finds the profile ID of the corresponding map code
-    df1 = pd.read_csv('WISE30sec/Interchangeable_format/HW30s_MapUnit.txt', sep=',', dtype=str)
-    print(df1)
-    print(str(map_code)[7:17])
-    soil_record = df1.loc[df1["NEWSUID"] == str(map_code)[7:17]]
-    profile_id = soil_record.get()
+    map_units = pd.read_csv('WISE30sec/Interchangeable_format/HW30s_MapUnit.txt', sep=',', dtype=str)
+    print(map_units)
+    print(map_code)
+    soil_record = map_units.loc[map_units["NEWSUID"] == map_code]
+    print(soil_record)
+    no_profiles = int(soil_record.get("NoSoilComp").iloc[0])
+    print(no_profiles)
+    profiles = {}
+    soil_map_unit = str(soil_record.get("SoilMapUnit"))[8:-32]
+    soil_climate_code = soil_map_unit[no_profiles*4:no_profiles*4+2]
+    print(soil_climate_code)
+    print(soil_map_unit)
+    if detailed == 1:
+        for i in range(no_profiles):
+            #profiles[soil_record.get]
+            pass
+    else:
+        dom_profile = str(soil_record.get("DomFAO_Name"))[8:10]
+        dom_prop = int(str(soil_record.get("DomFAO_Prop"))[7:10])
+        readProfile(soil_climate_code)
+
+        print(dom_profile)
+        print(dom_prop)
 
 
-getSoilData(0, 0, 1)
+
+#def readProfile(profile, )
+
+getSoilData(0, 0, 0)
