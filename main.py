1b74c412aef14be7c3970eedd8e69733410a9a40
RadiantFlame <46110688+RadiantFlame@users.noreply.github.com>
2023-05-10 05:19:13 +0100

    Set configurations for database and updated requirements.txt

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_ECHO'] = os.getenv('SQLALCHEMY_ECHO') == 'True'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') == 'True'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+db = SQLAlchemy(app)
