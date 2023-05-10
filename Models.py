8e62dfba03f201e98cf281ad11bb3557c829e9cf
RadiantFlame <46110688+RadiantFlame@users.noreply.github.com>
2023-05-10 08:11:54 +0100

    Added Fields Table to database

--- Models.py
+++ Models.py
@@ -4,13 +4,27 @@ from main import db, app
 class Users(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
+    fields = db.Relationship('Field', backref='user')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
+class Fields(db.Model):
+    __tablename__ = 'fields'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey("users.id"))
+    x = db.Column(db.Integer, nullable=False)
+    y = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 def initialiseDatabase():
