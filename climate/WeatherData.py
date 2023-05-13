83b121cb0adab5dd666ee40c13c2d612ecaf03f8
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2023-05-13 19:00:45 +0100

    function now works properly because found bug in coordstopixel function so deleted from this file

--- climate/WeatherData.py
+++ climate/WeatherData.py
@@ -2,22 +2,10 @@ from osgeo import gdal
 import numpy as np
 
 
-# this is hayden's code, I just copied it over until merge
-def coordsToPixels(long, lat, rows, cols):
-    long_max = 180
-    long_min = -180
-    lat_max = 90
-    lat_min = -90
-    assert long_min <= long <= long_max and lat_min <= lat <= lat_max
-    # converts the coords to their grid reference in the raster file
-    lat_idx = int((lat - lat_min) / (lat_max - lat_min) * rows)
-    long_idx = int((long - long_min) / (long_max - long_min) * cols)
-    return long_idx, lat_idx
-
-
 def getWeatherData(long, lat, folder):
     weather_dict = {
         'prec': [],
+        'srad': [],
         'temp_avg': [],
         'temp_max': [],
         'temp_min': [],
@@ -32,6 +20,7 @@ def getWeatherData(long, lat, folder):
 
     # gets the pixel coordinates
     x, y = coordsToPixels(long, lat, rows, cols)
+    print(x, y)
 
     # loops through the folders and files, and extracts data
     for category in weather_dict.keys():
@@ -42,12 +31,9 @@ def getWeatherData(long, lat, folder):
 
             # Get the pixel value at the given coordinates
             band = ds.GetRasterBand(1)
-            value = band.ReadAsArray(x, y, 1, 1)
+            value = band.ReadAsArray(x, y, 1, 1)[0][0]
 
             # Add the value to the data dictionary
             weather_dict[category].append(value)
 
     return weather_dict
-
-
-getWeatherData(6.852846021175841, -0.8454001119123822, 'tif_files')
