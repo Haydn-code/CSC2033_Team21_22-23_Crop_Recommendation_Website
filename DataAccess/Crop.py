b4cf1a39b9984ca395fc9e0ae3d03ca388ccf89b
Haydn Small <91317802+Haydn-code@users.noreply.github.com>
2023-05-14 16:26:45 +0100

    Created a file crops.py, that will handle interactions with the crop dataset, added a function getCrops which returns the necessary information from the crop datasets for the recommendation algorithm.

---
+++ DataAccess/Crop.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+"""This function returns the necessary info from the crop dataset for the recommendation algorithm
+
+Returns:
+A dictionary of crop_id(str) as keys, with a dictionary of relevant crop information as values.
+"""
+
+
+def getCrops():
+    crops = {}
+    # defines crop uses that address world hunger
+    valid_uses = ["vitamins", "protein", "fibres", "lipids/oils & fats", "condiment/seasoning", "minerals", "starch",
+                  "essential oils", "lipids", "honey", "sugar", "sweetener", "nutritional applications"]
+    plants = pd.read_csv('cropbasics_scrape.csv', sep=',', dtype=str)
+    common_names = pd.read_csv('crop_view_data.csv', sep=',', dtype=str)
+    for idx, row in plants.iterrows():
+        # prevents error from attempting to iterate over float values which can be found.
+        if type(row["Plant.attributes"]) == float:
+            pass
+        # checks that the crop can be grown on large scale and that its use addresses world hunger
+        elif row["use.detailed"] in valid_uses and "grown on large scale" in row["Plant.attributes"]:
+            # collects the relevant info for the crop and assigns to dict
+            info = {"optimal_max_temp": row["Temp_Opt_Max"], "absolute_max_temp": row["Temp_Abs_Max"],
+                    "optimal_min_temp": row["temp_opt_min"], "absolute_min_temp": row["Temp_Abs_Min"],
+                    "optimal_max_ph": row["pH_Opt_Max"], "absolute_max_ph": row["pH_Abs_Max"],
+                    "optimal_min_ph": row["pH_Opt_Min"], "absolute_min_ph": row["pH_Abs_Min"],
+                    "optimal_max_rain": row["Rain_Opt_Max"], "absolute_max_rain": row["Rain_Abs_Max"],
+                    "optimal_min_rain": row["Rain_Opt_Min"], "absolute_min_rain": row["Rain_Abs_Min"],
+                    "optimal_texture": row["Texture_Ops"], "absolute_texture": row["Texture_Abs"],
+                    "optimal_salinity": row["Salinity_Ops"], "absolute_salinity": row["Salinity_Abs"],
+                    "photoperiod": row["photoperiod"], "common_names":
+                        common_names.loc[common_names["Ecocrop_code"] == row["crop_code"]]["Common_names"].values[0]}
+
+            crops[row["crop_code"]] = info
+    # print(crops) # uncomment to see the return value
+    return crops
+
+
+getCrops()
