cb9398591e5843859dadcb9b16f5ebd66400e785
RadiantFlame <46110688+RadiantFlame@users.noreply.github.com>
2023-05-22 11:01:25 +0100

    Moved static folder to allow flask to use css file and images

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Frontend')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_ECHO'] = os.getenv('SQLALCHEMY_ECHO') == 'True'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') == 'True'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+app.static_folder = 'static'
+
+from users.Views import users_blueprint
+app.register_blueprint(users_blueprint)
+
 db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    app.run()
