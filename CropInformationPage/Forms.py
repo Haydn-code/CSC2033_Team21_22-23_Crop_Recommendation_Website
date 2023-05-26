7102b6d3979bbbd76286c1b711e7de677310a66b
Haydn Small <91317802+Haydn-code@users.noreply.github.com>
2023-05-26 17:33:16 +0100

    added a forms file that handles the search in the crop information page

---
+++ CropInformationPage/Forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class searchCropForm(FlaskForm):
+    search = StringField(validators=[DataRequired(), Length(max=50)])
+    submit = SubmitField()
