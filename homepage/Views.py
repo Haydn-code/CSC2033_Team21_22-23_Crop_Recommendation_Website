db3c822169827b3442307d7f99fa93a158a832ff
alex robertson <c0037654@newcastle.ac.uk>
2023-05-30 20:13:34 +0100

    Changed picture and button on donation section of home page.

--- homepage/Views.py
+++ homepage/Views.py
@@ -6,5 +6,5 @@ homepage_blueprint = Blueprint('Homepage', __name__, template_folder='Frontend')
 @homepage_blueprint.route('/')
 def homepage():
     about_image = url_for('static', filename='Projectimg/homepage_image_about2.jpg')
-    donation_image = url_for('static', filename='Projectimg/donate.jpg')
+    donation_image = url_for('static', filename='Projectimg/home-donate.png')
     return render_template('Homepage/index.html', image1=about_image, image2=donation_image)
